09bee8b91574ef187ed65c3c55a29cbc166b0d9c
Angela <aorceradw@gmail.com>
2026-03-16 10:19:05 +0100

    Primer commit: inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Haz clic en los logos de Vite y React para saber más.
+      </p>
+    </div>
+  );
+}
+
+export default App;

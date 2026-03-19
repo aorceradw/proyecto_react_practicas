cc5f775414aa2a7c7389eeafd7ae8adb35d18e64
Angela <aorceradw@gmail.com>
2026-03-19 12:42:05 +0100

    Actualizo app añadiendo el claro oscuro y la pagina de contactos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ModoClaroOscuro from './components/ModoClaroOscuro';
 
 import Home from './pages/Home';
 import Work from './pages/Work';
 import SobreMi from './pages/Sobre-Mi';
+import Contacto from './pages/Contacto';
 
 export default function App() {
     return (
@@ -16,9 +18,11 @@ export default function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/trabajos" element={<Work />} />
                     <Route path="/sobre-mi" element={<SobreMi />} />
+                    <Route path="/contacto" element={<Contacto />} />
                 </Routes>
             </main>
             <Footer />
+            <ModoClaroOscuro />
         </BrowserRouter>
     );
 }

7ad045afdf9b78f76d044fe01de7b0d14279d073
Angela <aorceradw@gmail.com>
2026-03-26 12:37:28 +0100

    componente para subir arriba al llegar al final de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 import Header   from './components/Header';
 import Footer   from './components/Footer';
+import SubirArriba from './components/SubirArriba';
 
 import Home     from './pages/Home';
 import Work     from './pages/Work';
@@ -45,6 +46,7 @@ export default function App() {
                 <AnimatedRoutes />
             </div>
             <Footer />
+            <SubirArriba />
         </BrowserRouter>
     );
 }
